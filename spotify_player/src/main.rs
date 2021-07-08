939916f31285099d5560d03999ac60c5dc904a39
Thang Pham <phamducthang1234@gmail.com>
2021-07-08 23:29:10 +0900

    experiment with `rspotify` using current_playing example

--- spotify_player/src/main.rs
+++ spotify_player/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use rspotify::client::Spotify;
+use rspotify::oauth2::{SpotifyClientCredentials, SpotifyOAuth};
+use rspotify::util::get_token;
+
+const SCOPES: [&str; 10] = [
+    "user-read-recently-played",
+    "user-top-read",
+    "user-read-playback-position",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+    "user-read-currently-playing",
+    "streaming",
+    "playlist-read-private",
+    "playlist-read-collaborative",
+    "user-library-read",
+];
+
+#[tokio::main]
+async fn main() {
+    let mut oauth = SpotifyOAuth::default().scope(&SCOPES.join(" ")).build();
+    match get_token(&mut oauth).await {
+        Some(token_info) => {
+            let client_credential = SpotifyClientCredentials::default()
+                .token_info(token_info)
+                .build();
+            let spotify = Spotify::default()
+                .client_credentials_manager(client_credential)
+                .build();
+            let result = spotify.current_playing(None, None).await;
+            println!("{:?}", result);
+        }
+        None => println!("auth failed"),
+    };
 }
