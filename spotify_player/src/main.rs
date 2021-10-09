af25694d9a14fd140fcbf3429337684703e6918f
Thang Pham <phamducthang1234@gmail.com>
2021-10-09 17:12:00 -0400

    release v0.3.0

--- spotify_player/src/main.rs
+++ spotify_player/src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> anyhow::Result<()> {
 
     // parse command line arguments
     let matches = clap::App::new("spotify-player")
-        .version("0.2.0")
+        .version("0.3.0")
         .about("A command driven spotify player")
         .author("Thang Pham <phamducthang1234@gmail>")
         .arg(
