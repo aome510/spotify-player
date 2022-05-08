105ed5384799d9a1a8ec7a2e61ab50dd85a9f4fa
Thang Pham <phamducthang1234@gmail.com>
2022-05-08 15:00:49 -0400

    remove `html2text` and add a base implementation for `parse` function

--- lyric_finder/src/lib.rs
+++ lyric_finder/src/lib.rs
@@ -2,8 +2,6 @@
 //!
 //! This crate provides Rust APIs to retrieve a song lyric.
 
-use anyhow::Result;
-
 const SEARCH_BASE_URL: &str = "https://genius.com/api/search";
 
 pub struct Client {
@@ -17,7 +15,7 @@ impl Client {
         }
     }
 
-    pub async fn search_lyric_urls(&self, query: &str) -> Result<Vec<String>> {
+    pub async fn search_lyric_urls(&self, query: &str) -> anyhow::Result<Vec<String>> {
         let body = self
             .http
             .get(format!("{SEARCH_BASE_URL}?q={query}"))
@@ -48,16 +46,57 @@ impl Client {
         }
     }
 
-    pub async fn get_lyric(&self, query: &str) -> Result<String> {
-        let urls = self.search_lyric_urls(query).await?;
-        println!("get a URL: {urls:?}");
+    pub async fn retrieve_lyric(&self, url: &str) -> anyhow::Result<String> {
+        let html = self.http.get(url).send().await?.text().await?;
+
+        println!("content: {html}");
         Ok(String::new())
+    }
+
+    pub async fn get_lyric(&self, query: &str) -> anyhow::Result<String> {
+        let urls = self.search_lyric_urls(query).await?;
+        self.retrieve_lyric(&urls[0]).await
     }
 }
 
 impl Default for Client {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+mod parse {
+    use html5ever::tendril::TendrilSink;
+    use html5ever::*;
+    use markup5ever_rcdom::{Handle, NodeData, RcDom};
+
+    pub fn parse(html: String) -> anyhow::Result<()> {
+        // parse HTML content into DOM node(s)
+        let dom = parse_document(RcDom::default(), Default::default())
+            .from_utf8()
+            .read_from(&mut (html.as_bytes()))?;
+
+        Ok(())
+    }
+
+    fn parse_dom_node(node: Handle) {
+        match &node.data {
+            NodeData::Text { contents } => {
+                todo!()
+            }
+            NodeData::Element {
+                ref name,
+                ref attrs,
+                ..
+            } => {
+                todo!()
+            }
+            _ => {}
+        }
+
+        node.children.borrow().iter().for_each(|node| {
+            parse_dom_node(node.clone());
+        });
     }
 }
 
