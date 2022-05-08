3befa0b6a1f8d5645f8ac1a85ec876425d713b1d
Thang Pham <phamducthang1234@gmail.com>
2022-05-08 14:35:35 -0400

    implement `search_lyric_urls` function

--- lyric_finder/examples/lyric-finder.rs
+++ lyric_finder/examples/lyric-finder.rs
@@ -2,8 +2,10 @@ extern crate lyric_finder;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let args = std::env::args().collect::<Vec<_>>();
+
     let client = lyric_finder::Client::new();
-    let lyric = client.get_lyric("bts").await?;
+    let lyric = client.get_lyric(&args[1]).await?;
     println!("lyric: {}", lyric);
 
     Ok(())
--- lyric_finder/src/lib.rs
+++ lyric_finder/src/lib.rs
@@ -4,7 +4,7 @@
 
 use anyhow::Result;
 
-const BASE_REQUEST_URL: &str = "https://genius.com/api/search/multi?per_page=5";
+const SEARCH_BASE_URL: &str = "https://genius.com/api/search";
 
 pub struct Client {
     http: reqwest::Client,
@@ -17,18 +17,79 @@ impl Client {
         }
     }
 
-    pub async fn get_lyric<S>(&self, query: S) -> Result<String>
-    where
-        S: std::fmt::Display,
-    {
-        let text = self
+    pub async fn search_lyric_urls(&self, query: &str) -> Result<Vec<String>> {
+        let body = self
             .http
-            .get(format!("{}&q={}", BASE_REQUEST_URL, query))
+            .get(format!("{SEARCH_BASE_URL}?q={query}"))
             .send()
             .await?
-            .text()
+            .json::<search::Body>()
             .await?;
 
-        Ok(text)
+        if body.meta.status != 200 {
+            let message = match body.meta.message {
+                Some(m) => m,
+                None => format!("request failed with status code: {}", body.meta.status),
+            };
+            return Err(anyhow::anyhow!(message));
+        }
+
+        let urls = body.response.map(|r| {
+            r.hits
+                .into_iter()
+                .filter(|hit| hit.ty == "song")
+                .map(|hit| hit.result.url)
+                .collect::<Vec<_>>()
+        });
+
+        match urls {
+            Some(v) => Ok(v),
+            None => Err(anyhow::anyhow!("lyric not found for query {}", query)),
+        }
+    }
+
+    pub async fn get_lyric(&self, query: &str) -> Result<String> {
+        let urls = self.search_lyric_urls(query).await?;
+        println!("get a URL: {urls:?}");
+        Ok(String::new())
+    }
+}
+
+impl Default for Client {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+mod search {
+    use serde::Deserialize;
+
+    #[derive(Debug, Deserialize)]
+    pub struct Body {
+        pub meta: Metadata,
+        pub response: Option<Response>,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct Metadata {
+        pub status: u16,
+        pub message: Option<String>,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct Response {
+        pub hits: Vec<Hit>,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct Hit {
+        #[serde(rename(deserialize = "type"))]
+        pub ty: String,
+        pub result: Result,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct Result {
+        pub url: String,
     }
 }
