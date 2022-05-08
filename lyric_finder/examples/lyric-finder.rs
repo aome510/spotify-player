9afbc6648d5cfcd20ed24c7d72dd5e5a8fe233e6
Thang Pham <phamducthang1234@gmail.com>
2022-05-08 14:39:00 -0400

    update example

--- lyric_finder/examples/lyric-finder.rs
+++ lyric_finder/examples/lyric-finder.rs
@@ -4,6 +4,11 @@ extern crate lyric_finder;
 async fn main() -> anyhow::Result<()> {
     let args = std::env::args().collect::<Vec<_>>();
 
+    if args.len() < 2 {
+        println!("Please specify the first argument to be the search query");
+        std::process::exit(1);
+    }
+
     let client = lyric_finder::Client::new();
     let lyric = client.get_lyric(&args[1]).await?;
     println!("lyric: {}", lyric);
