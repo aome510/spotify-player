fa9f4794c6284fc7b2869f7cec208e54e2288e7b
Thang Pham <phamducthang1234@gmail.com>
2022-06-04 22:30:14 -0400

    re-render image when changing theme

--- spotify_player/src/event/popup.rs
+++ spotify_player/src/event/popup.rs
@@ -158,6 +158,12 @@ pub fn handle_key_sequence_for_popup(
                 ui,
                 n_items,
                 |ui: &mut UIStateGuard, id: usize| {
+                    #[cfg(feature = "cover")]
+                    {
+                        // Changing the theme also clear the previously rendered image,
+                        // so we need to re-render it by reseting the below variable.
+                        ui.last_rendered_cover_image_url = String::new();
+                    }
                     ui.theme = match ui.popup {
                         Some(PopupState::ThemeList(ref themes, _)) => themes[id].clone(),
                         _ => return,
@@ -168,6 +174,12 @@ pub fn handle_key_sequence_for_popup(
                     Ok(())
                 },
                 |ui: &mut UIStateGuard| {
+                    #[cfg(feature = "cover")]
+                    {
+                        // Changing the theme also clear the previously rendered image,
+                        // so we need to re-render it by reseting the below variable.
+                        ui.last_rendered_cover_image_url = String::new();
+                    }
                     ui.theme = match ui.popup {
                         Some(PopupState::ThemeList(ref themes, _)) => themes[0].clone(),
                         _ => return,
