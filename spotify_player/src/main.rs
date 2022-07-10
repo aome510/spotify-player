fa61910abcace01cca4c498f8bd8d409a495f00d
Thang Pham <phamducthang1234@gmail.com>
2022-07-09 22:42:16 -0400

    bump app's version to 0.9.3

--- spotify_player/src/main.rs
+++ spotify_player/src/main.rs
@@ -18,7 +18,7 @@ use std::io::Write;
 
 fn init_app_cli_arguments() -> clap::ArgMatches {
     clap::Command::new("spotify-player")
-        .version("0.9.2")
+        .version("0.9.3")
         .about("A command driven spotify player")
         .author("Thang Pham <phamducthang1234@gmail>")
         .arg(
