ede65e5dc14fc55a70b48c6b9f54c82016da863e
Thang Pham <phamducthang1234@gmail.com>
2022-12-24 18:06:32 -0500

    use `static` instead of `const` when constructing `once_cell::Lazy<TracksId>`

--- spotify_player/src/state/consant.rs
+++ spotify_player/src/state/consant.rs
@@ -1,15 +1,15 @@
 pub use super::*;
 use once_cell::sync::Lazy;
 
-pub const USER_TOP_TRACKS_ID: Lazy<TracksId> =
+pub static USER_TOP_TRACKS_ID: Lazy<TracksId> =
     Lazy::new(|| TracksId::new("tracks:user-top-tracks", "Top Tracks"));
 
-pub const USER_RECENTLY_PLAYED_TRACKS_ID: Lazy<TracksId> = Lazy::new(|| {
+pub static USER_RECENTLY_PLAYED_TRACKS_ID: Lazy<TracksId> = Lazy::new(|| {
     TracksId::new(
         "tracks:user-recently-played-tracks",
         "Recently Played Tracks",
     )
 });
 
-pub const USER_LIKED_TRACKS_ID: Lazy<TracksId> =
+pub static USER_LIKED_TRACKS_ID: Lazy<TracksId> =
     Lazy::new(|| TracksId::new("tracks:user-liked-tracks", "Liked Tracks"));
