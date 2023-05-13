e6eea98d3b2a4286d089bc859367e5aa9d5a02f9
Saihaj Law <saihajlaw@Saihajs-MacBook-Air.local>
2023-05-13 19:57:53 -0400

    First Release

--- Formula/SpotifyPlayer.rb
+++ Formula/SpotifyPlayer.rb
@@ -1,11 +1,11 @@
-class Spotifyplayer < Formula
-  desc "Cross-platform Text Expander written in Rust"
+class SpotifyPlayer < Formula
+  desc "A CLI Spotify Tool Written in Rust"
   homepage "https://github.com/aome510/spotify-player"
   url "https://github.com/slano-ls/homebrew-spotify-player/releases/latest/download/spotify-player-mac.tar.gz"
   sha256 "f0685151774566835bfe94bb68c8bb77517ad54eda32936e7364681463f748a5"
   version "0.1.0"
 
   def install
-    bin.install "Spotifyplayer"
+    bin.install "SpotifyPlayer"
   end
 end
