bd568dfb3880ea62a02979be0f6c2b1601f85542
Jordan Sweet <hello@jordandsweet.com>
2023-09-17 17:06:49 -0700

    Remove redudant code from PlayerConfig in streaming.rs

--- spotify_player/src/streaming.rs
+++ spotify_player/src/streaming.rs
@@ -174,11 +174,7 @@ pub fn new_connection(
             .to_string()
             .parse::<Bitrate>()
             .unwrap_or_default(),
-	normalisation: device
-	    .normalization
-	    .to_string()
-	    .parse::<bool>()
-	    .unwrap_or_default(),
+        normalisation: device.normalization,
         ..Default::default()
     };
 
