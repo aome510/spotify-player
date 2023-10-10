bd1659a70ace449e90efbd25a543d17ad9df02f6
Icelk <main@icelk.dev>
2023-10-10 01:44:57 +0000

    Improve lyrics lookup by removing remix & remaster info in query. (#266)
    Co-authored-by: Thang Pham <phamducthang1234@gmail.com>

--- lyric_finder/src/lib.rs
+++ lyric_finder/src/lib.rs
@@ -58,6 +58,8 @@ impl Client {
 
     /// Search songs satisfying a given `query`.
     pub async fn search_songs(&self, query: &str) -> anyhow::Result<Vec<search::Result>> {
+        let query = improve_query(query);
+
         log::debug!("search songs: query={query}");
 
         let body = self
@@ -134,6 +136,66 @@ impl Default for Client {
     fn default() -> Self {
         Self::new()
     }
+}
+
+/// Returns `query` without `remaster` & `remix` information from track/artist query.
+/// Returned value is lowercase.
+/// These caused wildly invalid lyrics to be found.
+/// (try yourself adding remastered 2011 to a song's name when searching in Genius!)
+fn improve_query(query: &str) -> String {
+    // flag for doing something wrong if the song name (after removing remix metadata) is too short.
+    const SONG_MIN_LENGTH_WO_REMIX_METADATA: usize = 3;
+
+    let is_dash = |c: char| c == '-';
+
+    // reverse finder for non-filler (space, dashes) chars before an index.
+    // Acts like a trim to remove undesired spaces and dashes.
+    let rfind_non_filler = |s: &str, idx: usize| {
+        let Some(s) = s.get(..idx) else { return idx };
+        s.char_indices()
+            .rfind(|(_, c)| !(is_dash(*c) || c.is_whitespace()))
+            .map_or(idx, |(idx, c)| idx + c.len_utf8())
+    };
+    // used to handle longer variants of words: `remixed`, `remastered`, etc.
+    let end_of_word = |s: &str, idx: usize| {
+        let Some(s) = s.get(idx..) else { return idx };
+        s.find(|c: char| !c.is_alphanumeric())
+            .map_or(idx, |found| found + idx)
+    };
+
+    let mut query = query.to_lowercase();
+    // remove "xxxx Remaster" from the query
+    // For example, `{song} xxxx Remastered {artists}` becomes `{song} {artists}`.
+    if let Some(remaster_start) = query.find("remaster") {
+        let end = remaster_start + "remaster".len();
+        let end = end_of_word(&query, end);
+
+        let mut start = remaster_start.saturating_sub(1);
+        let prev = query.get(..remaster_start.saturating_sub(2)).unwrap_or("");
+        let end_of_prev_word = prev.rfind(' ').unwrap_or(0);
+
+        if let Some(year) = query.get(end_of_prev_word + 1..remaster_start.saturating_sub(1)) {
+            if year.chars().all(|c| c.is_whitespace() || c.is_numeric()) {
+                start = end_of_prev_word;
+            }
+        }
+        start = rfind_non_filler(&query, start);
+        query.drain(start..end);
+    }
+    // remove "- xxxx yyy remix" from the query
+    // For example, `{song} - xxxx yyy remix {artists}` becomes `{song} {artists}`.
+    if let Some(remix_start) = query.find("remix") {
+        let end = remix_start + "remix".len();
+        let end = end_of_word(&query, end);
+
+        if let Some(metadata_start) = query.rfind(is_dash) {
+            if metadata_start >= SONG_MIN_LENGTH_WO_REMIX_METADATA {
+                let start = rfind_non_filler(&query, metadata_start);
+                query.drain(start..end);
+            }
+        }
+    }
+    query
 }
 
 mod parse {
