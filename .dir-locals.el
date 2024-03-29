670fcbeec4b0c62699d45f5d7aaaf211a861f602
Thang Pham <phamducthang1234@gmail.com>
2024-03-28 22:59:59 -0400

    add patch for null issue with `images` field when getting user playlist (#402)
    Resolves #401

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((rustic-mode . ((lsp-rust-features . ["lyric-finder" "image" "notify" "clipboard"]))))
+((rustic-mode . ((eglot-workspace-configuration
+                  . (:rust-analyzer (:cargo (:features ["lyric-finder" "image" "notify" "clipboard"])
+                                     :check (:command "clippy")))))))
