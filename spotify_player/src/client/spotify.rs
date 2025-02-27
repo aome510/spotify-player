197be3e0224635ca07532ff9d17e93172a7b65bc
Alex Yong <alexjyong@gmail.com>
2025-02-27 21:33:05 +0000

    .

--- spotify_player/src/client/spotify.rs
+++ spotify_player/src/client/spotify.rs
@@ -85,7 +85,10 @@ impl Spotify {
         }
 
         // If the token is expired, we get a new one
-        if token_guard.as_ref().map_or(true, rspotify::Token::is_expired) {
+        if token_guard
+            .as_ref()
+            .map_or(true, rspotify::Token::is_expired)
+        {
             tracing::info!("Token expired, restarting session...");
             //trying out this awful hack
 
