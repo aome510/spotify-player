90aa1790b4071a8b7ab2205d832c7f2516399fbf
Thang Pham <phamducthang1234@gmail.com>
2025-06-22 15:12:15 -0400

    fmt

--- spotify_player/src/ui/page.rs
+++ spotify_player/src/ui/page.rs
@@ -289,7 +289,7 @@ pub fn render_context_page(
         Some(context) => {
             // render context description
             let chunks = Layout::vertical([Constraint::Length(1), Constraint::Fill(0)]).split(rect);
-            let is_followed_string = if let Context::Playlist { playlist, ..} = context {
+            let is_followed_string = if let Context::Playlist { playlist, .. } = context {
                 if data.user_data.is_followed_playlist(playlist) {
                     "Followed"
                 } else {
@@ -298,13 +298,14 @@ pub fn render_context_page(
             } else {
                 ""
             };
-                
+
             frame.render_widget(
                 Paragraph::new(format!(
                     "{} | {}",
                     context.description(),
                     is_followed_string
-                )).style(ui.theme.page_desc()),
+                ))
+                .style(ui.theme.page_desc()),
                 chunks[0],
             );
             let rect = chunks[1];
