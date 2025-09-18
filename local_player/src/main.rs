02eae1a8d97b4328329ffc971789ae5b54a6fc73
Mani Arasteh <mani.arasteh92@gmail.com>
2025-09-18 12:53:05 +0330

    Fixed the formatting, not important by the way.

--- local_player/src/main.rs
+++ local_player/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello from local_player member of spotify_player. The player is not implemented yet!");
+    println!(
+        "Hello from local_player member of spotify_player. The player is not implemented yet!"
+    );
 }
